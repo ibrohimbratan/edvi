ff6e09c6fa67f301310afcafc19b46feba9993ee
ibrohim <ibrohim.bratan97@gmail.com>
2021-06-07 16:00:52 +0500

    nojjknfe

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+
+
+$(function (){
+
+
+  $('.carousel').owlCarousel({
+    loop: true,
+    responsiveClass:true,
+    nav: false,
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      768:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:3,
+        nav:true,
+        loop:false
+      }
+    }
+  });
+
+});
+
+
